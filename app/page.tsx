e27f3844cf1c5940ec810950e97f80abb148c7c4
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-08-07 22:58:45 -0400

    Scaffolding UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      {/* Header */}
+      <h1 className="w-full text-center bg-blue-600">Guess the Country!</h1>
+      <h2 className="w-full text-center bg-blue-600">
+        Can you guess the country based on its capital city and flag?
+      </h2>
+
+      {/* Hints */}
+      <div className="w-full text-center bg-yellow-600">Capital: </div>
+      <div className="w-full text-center bg-yellow-600">Facts: </div>
+      <div className="w-full text-center bg-yellow-600">Flag</div>
+
+      {/* Input */}
+      <form>
+        <input placeholder="Guess the country!" />
+        <button>Guess</button>
+      </form>
+    </div>
+  );
 }
