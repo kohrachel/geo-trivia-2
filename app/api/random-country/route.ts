3ee3d14bddc96863110a8aa90dc23f72246c6c74
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-08-08 15:47:13 -0400

    Don't return country name to the frontend

--- app/api/random-country/route.ts
+++ app/api/random-country/route.ts
@@ -5,5 +5,11 @@ export async function GET() {
   const country = COUNTRIES[Math.floor(Math.random() * COUNTRIES.length)];
   setCorrectCountry(country);
 
-  return Response.json(country);
+  const { flags: flag, capital: capitalsList } = country;
+  const frontendCountryInfo = {
+    flag: { ...flag },
+    capitalsList: [...capitalsList],
+  };
+
+  return Response.json(frontendCountryInfo);
 }
