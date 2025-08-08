d69ec2bfc4297044fa8e6888a6d1773d76d15ad8
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-08-08 15:35:57 -0400

    Listen I got it to actually regenerate a new country on load omggggggggg

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,51 @@
+"use client";
+
 import React, { useEffect } from "react";
 import Game from "./components/Game";
 import { cookies } from "next/headers";
 import { getRandomCountry } from "./helpers/getRandomCountry";
+import useSWR from "swr";
 
-export default async function Home() {
+export default function Home() {
   // Set the cookie only if it doesn't exist
-  const cookieStore = await cookies();
-  let correctCountry = cookieStore.get("correctCountry")?.value;
+  // const cookieStore = await cookies();
+  // let correctCountry = cookieStore.get("correctCountry")?.value;
 
-  if (!correctCountry) {
-    correctCountry = getRandomCountry().name.common;
+  // if (!correctCountry) {
+  //   correctCountry = getRandomCountry().name.common;
 
-    cookieStore.set("correctCountry", correctCountry, {
-      httpOnly: true,
-      path: "/",
-    });
-  }
+  //   cookieStore.set("correctCountry", correctCountry, {
+  //     path: "/",
+  //   });
+  // }
+  const { data, error, isLoading } = useSWR(
+    `/api/random-country`,
+    countryFetcher
+  );
 
-  // Render your game UI (client component for interactivity)
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
+  const gameCountry = {
+    flag: {
+      png: "https://flagcdn.com/w320/md.png",
+      svg: "https://flagcdn.com/md.svg",
+      alt: "The flag of Moldova is composed of three equal vertical bands of blue, yellow and red, with the national coat of arms centered in the yellow band.",
+    },
+    capitalsList: ["Chișinău"],
+  };
+
   return (
     <div>
       <h1>Guess the Country Game</h1>
-      {/* Render your client-side game component here */}
-      <Game />
+      <Game country={gameCountry} />
     </div>
   );
 }
+
+const countryFetcher = async (endpoint: string) => {
+  const response = await fetch(endpoint);
+  const json = await response.json();
+
+  return json;
+};
