c4676942bb8dc5f0862a03a37758435d7f67c697
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-08-08 14:30:34 -0400

    Initialize correctCountry to non-undefined value (may undo this later)

--- app/api/database.ts
+++ app/api/database.ts
@@ -19,7 +19,28 @@ export type countryType = {
   capital: string[];
 };
 
-let correctCountry: countryType;
+let correctCountry: countryType = {
+  flags: {
+    png: "https://flagcdn.com/w320/my.png",
+    svg: "https://flagcdn.com/my.svg",
+    alt: "The flag of Malaysia is composed of fourteen equal horizontal bands of red alternating with white. A blue rectangle, bearing a fly-side facing yellow crescent and a fourteen-pointed yellow star placed just outside the crescent opening, is superimposed in the canton.",
+  },
+  name: {
+    common: "Malaysia",
+    official: "Malaysia",
+    nativeName: {
+      eng: {
+        official: "Malaysia",
+        common: "Malaysia",
+      },
+      msa: {
+        official: "مليسيا",
+        common: "مليسيا",
+      },
+    },
+  },
+  capital: ["Kuala Lumpur"],
+};
 
 export const setCorrectCountry = (country: countryType) => {
   correctCountry = country;
