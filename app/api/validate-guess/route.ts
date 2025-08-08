b9ec045873a04ff2a4299ae4b8e45ac3e81247f2
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-08-08 14:47:07 -0400

    Perhaps this works?

--- app/api/validate-guess/route.ts
+++ app/api/validate-guess/route.ts
@@ -1,10 +1,9 @@
-// { guessedCountry: string }
-import { correctCountry } from "../database";
+import { getCorrectCountry } from "../database";
 
 export async function POST(req: Request) {
   const { guessedCountry } = await req.json();
   const guessedCountryLower = JSON.stringify(guessedCountry).toLowerCase();
-  const correctCountryLower = correctCountry.toLowerCase();
+  const correctCountryLower = getCorrectCountry().name.common.toLowerCase();
 
   const correct = guessedCountryLower.includes(correctCountryLower);
   return Response.json({ correctGuess: correct });
